b76eff0f9bbb84ce43ff1de1df443d63eb7625ba
JongKook Kim <d0nzs00n@gmail.com>
2022-03-08 14:22:43 +0900

    change AppLifecycleReactor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
 
+## 0.0.5 - 2022-03-08
+### Changed
+- AppLifecycleReactor 이벤트 핸들링 방식 
+  WidgetsBindingObserver => AppStateEventNotifier 변경
+
 ## 0.0.4 - 2022-02-26
 ### Changed
 - google_mobile_ads: ^1.0.1 -> google_mobile_ads: ^1.1.0
--- lib/src/app_lifecycle_reactor.dart
+++ lib/src/app_lifecycle_reactor.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app_open_ad_manager.dart';
 
+/* 
+https://github.com/googleads/googleads-mobile-flutter/blob/main/packages/app_open_example/lib/app_lifecycle_reactor.dart
+구글 예제에서 WidgetBindingOvserver에서 AppStateEventNotifier로 변경됨.
+아마도 광고사기(ad fraud)와 관련된 문제일 것으로 추측되어 동일하게 바꿈
+
+WidgetBindingOvserver 사용방식
 class AppLifecycleReactor extends WidgetsBindingObserver {
   final AppOpenAdManager appOpenAdManager;
 
@@ -13,6 +20,29 @@ class AppLifecycleReactor extends WidgetsBindingObserver {
     // we're not alreay showing an app open ad.
     if (state == AppLifecycleState.resumed) {
       print('AppLifecycleState.resumed, showAdIfAvailable');
+      appOpenAdManager.showAdIfAvailable();
+    }
+  }
+}
+*/
+
+class AppLifecycleReactor {
+  final AppOpenAdManager appOpenAdManager;
+
+  AppLifecycleReactor({required this.appOpenAdManager});
+
+  void listenToAppStateChanges() {
+    AppStateEventNotifier.startListening();
+    AppStateEventNotifier.appStateStream
+        .forEach((state) => _onAppStateChanged(state));
+  }
+
+  void _onAppStateChanged(AppState state) {
+    print('New AppState state: $state');
+
+    // Try to show an app open ad if the app is being forground and
+    // We're not alreay showing an app open ad.
+    if (state == AppState.foreground) {
       appOpenAdManager.showAdIfAvailable();
     }
   }
